89b30c9bf7867d2df82293915bed852ef5ea30a8
Robinson Betancur Marin <robinson.betancur@sinapsis.global>
2024-01-14 16:38:08 -0500

    redesign login

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './routes/Login.tsx';
-import { ProfileView } from './routes/ProfileView.tsx';
 import { RoutesDashboard } from './routes/RoutesDashboard.tsx';
 import Home from './routes/Home.tsx';
 import ReactDOM from 'react-dom/client';
@@ -27,7 +26,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             />
             <Route path='admin' element={<Login />} />
             <Route path='dashboard' element={<RoutesDashboard />} />
-            <Route path='profile/:username' element={<ProfileView />} />
         </Routes>
     </BrowserRouter>
 );
