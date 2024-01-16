f4ae02862153aa194e8e649552f4feb88aab8861
Robinson Betancur Marin <robinson.betancur@sinapsis.global>
2024-01-15 19:49:08 -0500

    fixbug nextUI and create new Route

--- src/main.tsx
+++ src/main.tsx
@@ -4,28 +4,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Login } from './routes/Login.tsx';
-import { RoutesDashboard } from './routes/RoutesDashboard.tsx';
-import Home from './routes/Home.tsx';
-import ReactDOM from 'react-dom/client';
-import { NavbarMenu } from './components/NavbarMenu.tsx';
 import { Footer } from './components/Footer.tsx';
+import { Home } from './routes/Home.tsx';
+import { HotelsView } from './routes/HotelsView.tsx';
+import { Login } from './routes/Login.tsx';
+import { NavbarMenu } from './components/NavbarMenu.tsx';
+import { NextUIProvider } from '@nextui-org/react';
+import { RoutesDashboard } from './routes/RoutesDashboard.tsx';
+import ReactDOM from 'react-dom/client';
+import { HotelsProvider } from './context/HotelsProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
-        <Routes>
-            <Route
-                path='/'
-                element={
-                    <>
-                        <NavbarMenu />
-                        <Home />
-                        <Footer />
-                    </>
-                }
-            />
-            <Route path='admin' element={<Login />} />
-            <Route path='dashboard' element={<RoutesDashboard />} />
-        </Routes>
+        <HotelsProvider>
+            <NextUIProvider>
+                <Routes>
+                    <Route
+                        path='/'
+                        element={
+                            <>
+                                <NavbarMenu />
+                                <Home />
+                                <Footer />
+                            </>
+                        }
+                    />
+                    <Route
+                        path='/'
+                        element={
+                            <>
+                                <NavbarMenu />
+                                <HotelsView />
+                                <Footer />
+                            </>
+                        }
+                    />
+                    <Route path='admin' element={<Login />} />
+                    <Route path='dashboard/*' element={<RoutesDashboard />} />
+                </Routes>
+            </NextUIProvider>
+        </HotelsProvider>
     </BrowserRouter>
 );
