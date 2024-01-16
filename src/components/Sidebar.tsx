3e4b4502ef6ac01e322ac4ac66ea5f06043c241a
Robinson Betancur Marin <robinson.betancur@sinapsis.global>
2024-01-15 19:50:33 -0500

    change some styles and refactory buttonRedirect

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,21 @@
+import { faBed, faDollar, faDoorOpen, faHotel } from '@fortawesome/free-solid-svg-icons';
+import '../styles/components/Sidebar.scss';
 import { ButtonRedirect } from './share/ButtonRedirect';
+import { Link } from 'react-router-dom';
 
 export const Sidebar = () => {
     return (
         <>
-            <div className='w-100 d-flex flex-column align-items-center'>
-                <h2>Hoteles Ultra</h2>
-                <ButtonRedirect text='Gestionar Hoteles' url='/' />
-                <ButtonRedirect text='Habitaciones disponibles' url='/' />
-                <ButtonRedirect text='Precios habitacion' url='/' />
-                <ButtonRedirect text='Gestionar Hoteles' url='/' />
+            <div className='sidebar-menu w-100 d-flex flex-column align-items-center'>
+                <Link to='/dashboard'>
+                    <h2>Hoteles Ultra</h2>
+                </Link>
+                <ButtonRedirect text='Gestionar Hoteles' url='/dashboard/hoteles' icon={faHotel} />
+                <ButtonRedirect text='Habitaciones disponibles' url='/' icon={faBed} />
+                <ButtonRedirect text='Precios habitacion' url='/' icon={faDollar} />
             </div>
             <div className='w-100'>
-                <ButtonRedirect text='Salir' url='/' />
+                <ButtonRedirect text='Salir' url='/' icon={faDoorOpen} />
             </div>
         </>
     );
--- src/components/share/ButtonRedirect.tsx
+++ src/components/share/ButtonRedirect.tsx
@@ -1,18 +1,23 @@
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useNavigate } from 'react-router-dom';
 
 interface ButtonRedirectProps {
     text: string;
     url: string;
+    icon?: IconDefinition;
 }
 
-export const ButtonRedirect: React.FC<ButtonRedirectProps> = ({ text, url }) => {
+export const ButtonRedirect: React.FC<ButtonRedirectProps> = ({ text, url, icon }) => {
     const navigate = useNavigate();
     const handlerRedirect = () => {
         navigate(url);
     };
-    console.log('RobinDev - text:', text);
     return (
-        <button className='btn my-2' onClick={handlerRedirect}>
+        <button
+            className={icon ? 'px-2 btn my-2 text-start' : 'btn my-2'}
+            onClick={handlerRedirect}>
+            {icon ? <FontAwesomeIcon className='me-3' icon={icon} /> : null}
             {text}
         </button>
     );
