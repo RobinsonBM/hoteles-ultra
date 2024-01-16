43fedae549064cc8bf9a3a71469940535c6e8bf5
Robinson Betancur Marin <robinson.betancur@sinapsis.global>
2024-01-15 19:52:53 -0500

    map destinies

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
 import { Link } from 'react-router-dom';
 import '../styles/components/Footer.scss';
+import { useHotelsContext } from '../context/HotelsProvider';
+import { useEffect, useState } from 'react';
 
 export const Footer = () => {
+    const [destinies, setDestinies] = useState<string[]>([]);
+    const { hotels } = useHotelsContext();
+
+    useEffect(() => {
+        if (hotels!.length > 0) getDestiny();
+    }, [hotels]);
+
+    const getDestiny = () => {
+        const destiniesSet = new Set(hotels!.map((hotel) => hotel.ubicacion));
+        const destinies = Array.from(destiniesSet).sort();
+        setDestinies(destinies);
+    };
+
     return (
         <div className='footer d-flex'>
             <Link className='footer-logotipo my-3' to='/'>
@@ -9,11 +24,19 @@ export const Footer = () => {
                 <h1>Hoteles Ultra</h1>
             </Link>
             <div className='footer-menu my-3'>
-                <Link to='/hoteles'>Hoteles</Link>
-                <Link to='/admin'>Admin</Link>
-                <Link to='/contacto'>Contactanos</Link>
+                <h5>Hoteles segun destino</h5>
+                {destinies.map((destiny, index) => {
+                    console.log('RobinDev - destiny:', destiny);
+                    return (
+                        <Link key={index} to='/hoteles'>
+                            Hoteles en {destiny}
+                        </Link>
+                    );
+                })}
             </div>
             <div className='footer-copyrights my-3'>
+                <Link to='/admin'>Admin</Link>
+                <Link to='/contacto'>Contactanos</Link>
                 <p>Todos los derechos reservados&reg; | Robinson Betancur Marin</p>
                 <p>Desarrollador de Software</p>
             </div>
