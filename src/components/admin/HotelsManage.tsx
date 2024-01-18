e4a7f1a8dd622ebd1028d1fa0a06221092a00ee7
Robinson Betancur Marin <robinson.betancur@sinapsis.global>
2024-01-18 16:20:08 -0500

    bind hotels in table admin

--- src/components/admin/HotelsManage.tsx
+++ src/components/admin/HotelsManage.tsx
@@ -1,47 +1,67 @@
-import '../../styles/components/admin/HotelsManage.scss';
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
+import "../../styles/components/admin/HotelsManage.scss";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/react";
+import { useHotelsContext } from "../../context/HotelsProvider";
+import { HotelModel } from "../../types";
 
 const columns = [
-    {
-        key: 'nombre',
-        label: 'Hoteles',
-    },
-    {
-        key: 'acciones',
-        label: 'Acciones',
-    },
+  {
+    key: "name",
+    label: "Hoteles",
+  },
+  {
+    key: "destiny",
+    label: "Acciones",
+  },
+  {
+    key: "email",
+    label: "Acciones",
+  },
+  {
+    key: "telephone",
+    label: "Telefono",
+  },
+  {
+    key: "actions",
+    label: "Acciones",
+  },
 ];
 
 export const HotelsManage = () => {
-    return (
-      <div className='hotels-manage'>
-            <div className=' header-manage d-flex justify-content-between align-items-center'>
-                <h3>Gestionar Hoteles</h3>
-                <button className='btn btn-secondary'>Nuevo Hotel</button>
-            </div>
-            <Table aria-label='Example static collection table'>
-                <TableHeader columns={columns}>
-                    {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-                </TableHeader>
-                <TableBody>
-                    <TableRow key='1'>
-                        <TableCell>Tony Reichert</TableCell>
-                        <TableCell>CEO</TableCell>
-                    </TableRow>
-                    <TableRow key='2'>
-                        <TableCell>Zoey Lang</TableCell>
-                        <TableCell>Technical Lead</TableCell>
-                    </TableRow>
-                    <TableRow key='3'>
-                        <TableCell>Jane Fisher</TableCell>
-                        <TableCell>Senior Developer</TableCell>
-                    </TableRow>
-                    <TableRow key='4'>
-                        <TableCell>William Howard</TableCell>
-                        <TableCell>Community Manager</TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </div>
-    );
+  const { hotels } = useHotelsContext();
+
+  return (
+    <div className="hotels-manage">
+      <div className=" header-manage d-flex justify-content-between align-items-center">
+        <h3>Gestionar Hoteles</h3>
+        <button className="btn btn-secondary">Nuevo Hotel</button>
+      </div>
+      <Table aria-label="Example static collection table" className="my-4">
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.key}>{column.label}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={hotels}>
+          {(item: HotelModel) => (
+            <TableRow key={item.id}>
+              {(columnKey) => (
+                <>
+                  <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+                  <TableCell>{item.id}</TableCell>
+                </>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
